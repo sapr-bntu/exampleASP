56a15e9226b2a131b537d57f5a8bf723d2f42817
Alex <s5005a@gmail.com>
2012-03-30 23:41:45 +0300

    в отладке заработало

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,68 +4,90 @@ var dust = require('dust');
 var sqlite3 = require('sqlite3').verbose();
 
 var opCount = 0;
-var content ="";
+//var content ="";
 var resp;
 function start(response) {
 
-    var db = new sqlite3.Database('stud.s3db');
-    resp = response;
 
 
-    db.each("select MAX(numlab) from labs", function(err, row) {
-                var conutLab = 0;
-                  for (var i in row) // обращение к свойствам объекта по индексу
-                      conutLab = row[i] ;
 
-                  for(var index = 1;index<=conutLab;index++)
-                  {
-                      content += "лаба "+ index +"<br>";
-                       var db2 = new sqlite3.Database('stud.s3db');
-                      db2.all("select link,name,idteam from labs where numlab = "+index, function(err2, row2) {
-                                  row2.forEach(function (row3) {
 
-                                   content += " "+row3.link+" "+ row3.name+"<br>";
-                                   console.log(" "+row3.link+" "+ row3.name+"<br>");
-                                               });
-                                  rend();
-                               ///);
-                      db2.close();
-                              });
-                  }
-            });
-    db.close();
+
+    var content ="main";
+    loadDb();
+
+    var text = fs.readFileSync('templates/main.html');
+    text =" "+ text;
+    var compiled = dust.compile(text, "main");
+    //    console.log(compiled);
+    dust.loadSource(compiled);
+    dust.render("main", {
+                    title: "Контакты",
+                    org:"БНТУ",
+                    about:"О проекте",
+                    contact:"Контакты",
+                    project:"САПР - БНТУ",
+                    home:"Главная",
+                    content:content
+                }, function(err, out) {
+                    //  Response.Write(out);
+
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.write(out);
+                    response.end();
+                    //                    console.log("render ok ");
+                });
+
+    function loadDb()
+    {
+        var db = new sqlite3.Database('stud.s3db');
+        db.each("select numlab from labs group by numlab", function(err, row) {//цикл 1: номера  лаб
+                    console.log("лаба номер  "+row.numlab);
+                    var db2 = new sqlite3.Database('stud.s3db');
+                    db2.each("select link,name,idteam from labs where numlab ="+row.numlab, function(err, row2) {//цикл 2:   лабы
+                                 console.log("строка $ "+row2.link+" "+row2.name+" "+row2.idteam+"  n"+row.numlab);
+                                 var db3 = new sqlite3.Database('stud.s3db');
+                                 db3.each("select firstname,lastname,'group' from students,teams where students.id =iduser AND idteam ="+row2.idteam, function(err, row3) {//цикл 3:   студенты
+                                              console.log("пользователь $ "+row3.firstname+" "+row3.lastname);
+                                          },function endDb3(){
+                                              console.log("+++++++++++++++++++++");
+                                              db3.close();
+                                          });
+                             },function endDb2(){
+                                 console.log("________________________");
+                                 db2.close();
+                             });
+                },function endDb1(){
+                    db.close();
+                });
+
+        //                var conutLab = 0;
+        //                  for (var i in row) // обращение к свойствам объекта по индексу
+        //                      conutLab = row[i] ;
+
+        //                  for(var index = 1;index<=conutLab;index++)
+        //                  {
+        //                      content += "лаба "+ index +"<br>";
+        //                       var db2 = new sqlite3.Database('stud.s3db');
+        //                      db2.all("select link,name,idteam from labs where numlab = "+index, function(err2, row2) {
+        //                                  row2.forEach(function (row3) {
+
+        //                                   content += " "+row3.link+" "+ row3.name+"<br>";
+        //                                   console.log(" "+row3.link+" "+ row3.name+"<br>");
+        //                                               });
+        //                                  rend();
+        //                               ///);
+        //                      db2.close();
+        //                              });
+        //                  }
+        //            });
+        //    db.close();
+        console.log("loadDb");
+    }
+
 
 }
-function rend(){
-    if(opCount>0)
-    {
-        console.log("ok" + opCount); opCount++
-                        var text = fs.readFileSync('templates/main.html');
-                        text =" "+ text;
-                        var compiled = dust.compile(text, "main");
-                        //    console.log(compiled);
-                        dust.loadSource(compiled);
-                        dust.render("main", {
-                                        title: "Программирование в интернет",
-                                        org:"БНТУ",
-                                        about:"О проекте",
-                                        contact:"Контакты",
-                                        project:"Программирование в интернет",
-                                        home:"Главная",
-                                        content:content
-                                    }, function(err, out) {
-                                        //  Response.Write(out);
-                                        resp.writeHead(200, {"Content-Type": "text/html"});
-                                        resp.write(out);
-                                        resp.end();
-                                        //                    console.log("render ok ");
-                                    });
-    }
-    else
-    {console.log("not" + opCount); opCount++}
 
-
-            }
 function contact(response) {
 
     var content ="Котнакты";
